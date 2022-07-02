fc94583dd40221d073cc77b0035e1435d4fed6e8
BSide Luke <bsideluke@gmail.com>
2022-07-03 00:24:33 +0100

    Lavalink link fixed

--- Lavalink/start.js
+++ Lavalink/start.js
@@ -2,6 +2,6 @@ import { spawn } from 'child_process';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const jar = `${dirname(fileURLToPath(import.meta.url))}\\Lavalink.jar`;
+const jar = `${dirname(fileURLToPath(import.meta.url))}/Lavalink.jar`;
 
 spawn('java', ['-jar', jar], { stdio: 'inherit' });
