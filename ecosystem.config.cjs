c11c349e6d132f96b83e52c71ceaf1754b50d7f4
BSide Luke <bsideluke@gmail.com>
2022-07-03 00:10:37 +0100

    Added pm2 ecosystem and Lavalink files

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+	apps: [
+		{
+            name: 'Lavalink',
+			script: 'npm',
+			args: 'run lavalink'
+		},
+		{
+            name: 'lkdj',
+			script: 'npm',
+			args: 'start'
+		}
+    ]
+};
